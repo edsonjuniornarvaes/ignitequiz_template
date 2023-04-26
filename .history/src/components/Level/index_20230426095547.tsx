e9489b99817d18c965df669a767b667d90eb4401
Edson Junior <edsonjunior@pop-os.localdomain>
2023-04-26 09:56:32 -0300

    style: add styles in useSharedValue

---
+++ .history/src/components/Level/index_20230426095547.tsx
@@ -0,0 +1,69 @@
+import { Pressable, PressableProps, Text, View } from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+
+import { THEME } from "../../styles/theme";
+import { styles } from "./styles";
+
+const TYPE_COLORS = {
+  EASY: THEME.COLORS.BRAND_LIGHT,
+  HARD: THEME.COLORS.DANGER_LIGHT,
+  MEDIUM: THEME.COLORS.WARNING_LIGHT,
+};
+
+type Props = PressableProps & {
+  title: string;
+  isChecked?: boolean;
+  type?: keyof typeof TYPE_COLORS;
+};
+
+export function Level({
+  title,
+  type = "EASY",
+  isChecked = false,
+  ...rest
+}: Props) {
+  const scale = useSharedValue(1);
+
+  const COLOR = TYPE_COLORS[type];
+
+  const animatedContainerStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: scale.value }],
+    };
+  });
+
+  function onPressIn() {
+    scale.value = 1.2;
+  }
+
+  function onPressOut() {
+    scale.value = 1;
+  }
+
+  return (
+    <Pressable onPressIn={onPressIn} onPressOut={onPressOut} {...rest}>
+      <Animated.View
+        style={[
+          styles.container,
+          animatedContainerStyle,
+          {
+            borderColor: COLOR,
+            backgroundColor: isChecked ? COLOR : "transparent",
+          },
+        ]}
+      >
+        <Text
+          style={[
+            styles.title,
+            { color: isChecked ? THEME.COLORS.GREY_100 : COLOR },
+          ]}
+        >
+          {title}
+        </Text>
+      </Animated.View>
+    </Pressable>
+  );
+}
